e70ec88f36f8d08a570aafb59ea70ec3f6000331
susmitamenka <susmitaprasad73@gmail.com>
2025-05-12 13:51:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+document.getElementById('btn').addEventListener('click', async () => {
+  const message = document.getElementById('text').value;
+  const delayTime = parseInt(document.getElementById('delay').value, 10);
+
+  if (message && !isNaN(delayTime)) {
+    await delay(delayTime);
+    document.getElementById('output').innerText = message;
+  } else {
+    alert('Please provide both a message and a valid delay time.');
+  }
+});
+
+
